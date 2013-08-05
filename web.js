ea7b44d1f34e8b5cb0fb86c629d7b9f1ea5e7999
Ramon Solorzano <culturetek@gmail.com>
2013-08-05 03:33:11 +0000

    readsync change to web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+   fs.readFileSync(index.html);   
+  response.send('buf.toString('utf8')');
 });
 
 var port = process.env.PORT || 5000;
